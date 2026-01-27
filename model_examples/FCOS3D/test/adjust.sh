bc88de2b94a53849b9f0c604729d7fa4668409a6
HITzengyz <2538239082@qq.com>
2026-01-27 20:38:06 +0800

    删除多余的项目

--- model_examples/FCOS3D/test/adjust.sh
+++
@@ -1,5 +0,0 @@
-pip uninstall termcolor -y
-pip install termcolor==2.4.0
-pip uninstall mmcv-full -y
-pip uninstall numpy -y
-pip install numpy==1.24.4
--- model_examples/Pi-0/test/paligemma_weights_mod.sh
+++
@@ -1,2 +0,0 @@
-paligemma_weights=$1
-sed -i "251s|self.language_tokenizer = AutoTokenizer.from_pretrained(\"google/paligemma-3b-pt-224\")|self.language_tokenizer = AutoTokenizer.from_pretrained(\"${paligemma_weights}\")|" lerobot/common/policies/pi0/modeling_pi0.py
