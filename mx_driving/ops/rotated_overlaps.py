a1c58eae125e3e507ca1d66cdb368b5f74679db0
HITzengyz <2538239082@qq.com>
2026-03-04 10:42:34 +0800

    Remove radar mode

--- mx_driving/ops/rotated_overlaps.py
+++
@@ -1,8 +0,0 @@
-"""
-Copyright (c) Huawei Technologies Co., Ltd. 2024. All rights reserved.
-"""
-import torch
-import torch_npu
-import mx_driving._C
-
-npu_rotated_overlaps = mx_driving._C.npu_rotated_overlaps
